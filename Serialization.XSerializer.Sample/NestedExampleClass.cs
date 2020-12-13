dbfd9ff46a26c6c715cd94663d2a8487ccf66564
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-13 10:19:18 +0330

    Add Serialization.XSerializer.Sample

---
+++ Serialization.XSerializer.Sample/NestedExampleClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Serialization.XSerializer.Sample
+{
+    public class NestedExampleClass
+    {
+        public int IntProperty { get; set; }
+        public bool BoolProperty { get; set; }
+        public string StringProperty { get; set; }
+    }
+}
---
+++ Serialization.XSerializer.Sample/ParentExampleClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Serialization.XSerializer.Sample
+{
+    public class ParentExampleClass
+    {
+        public DateTime DateProperty { get; set; }
+        public NestedExampleClass ClassProperty { get; set; }
+    }
+}
